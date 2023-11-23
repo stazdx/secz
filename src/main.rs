18670801460162f7593329ec170714ebfe6ba601
Staz <stanislav.93.18@gmail.com>
2023-11-23 00:38:01 -0500

    structOpts working!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use secz::cli;
+use structopt::StructOpt;
 
 fn main() {
     println!("Hello, world!");
-    let cli = cli::Cli {
-        pattern: "test".to_string(),
-        path: std::path::PathBuf::from("file.txt"),
-    };
+    // let cli = cli::Cli {
+    //     pattern: "test".to_string(),
+    //     path: std::path::PathBuf::from("file.txt"),
+    // };
+
+    let cli = cli::Cli::from_args();
 
     println!("{:?}", cli);
 
