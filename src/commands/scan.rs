4d74895c1f29861112c96abcb1fdb01986a4525e
Staz <stanislav.93.18@gmail.com>
2023-11-25 03:47:36 -0500

    adding progress bar in runtimme

--- src/commands/scan.rs
+++ src/commands/scan.rs
@@ -1,21 +1,34 @@
 
 use structopt::StructOpt;
 use std::process::Command;
+use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn sonarRun() {
-    let output = Command::new("sonar-scanner")
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(ProgressStyle::default_spinner());
+
+    let mut child = Command::new("sonar-scanner")
             .arg("-Dsonar.rust.clippy.reportPaths=clippy-report.json")
             .arg("-Dsonar.login=admin")
             .arg("-Dsonar.password=admin123")
-            .output()
+            // .output()
+            .spawn()
             .expect("Failed to execute command");
 
-    let output = String::from_utf8_lossy(&output.stdout);
+    pb.set_message("Running sonar-scanner...");
+    
+    while let Ok(None) = child.try_wait() {
+        pb.tick();
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 
-    println!("Output: {}", output);
+    pb.finish_with_message("sonarRun completed");
+
+    // let child = String::from_utf8_lossy(&child.stdout);
+
+    // println!("Output: {}", child);
 
 }
-
 
 #[derive(Debug, StructOpt)]
 pub enum ResourceType {
