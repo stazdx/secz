591c4a83532c617b2afd701fbd12dceb2d972729
Staz <stanislav.93.18@gmail.com>
2023-11-29 03:32:48 -0500

    refactorizing main.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use crate::commands::scan;
 
-use clap::{Subcommand, Parser, ArgMatches};
+use clap::{Subcommand, Parser};
 
 #[derive(Debug, Clone, Parser)]
 pub struct Cli {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// use structopt::StructOpt;
 use clap::{Command, Args};
 mod cli;
 
@@ -8,58 +7,14 @@ mod commands {
 
 fn main() {
     let cli = Command::new("secz");
-
     let command = cli::Cli::augment_args(cli).get_matches();
 
-    match command.subcommand_matches("scan") {
-        Some(sub_matches) => {
-            let tool = if sub_matches.subcommand_matches("sonar").is_some() {
-                "sonar"
-            } else {
-                "other"
-            };
-            println!("Tool: {:?}", tool);
-        }
-        None => {
-            eprintln!("Unknown command");
-        },
-    }
-}
-    // match command.subcommand() {
-    //     Some(("scan", sub_matches)) => {
-    //         println!("{}", command.);
-    //         let tool = if command.contains_id("sonar") {
-    //             "sonar"
-    //         } else {
-    //             "other"
-    //         };
-    //         println!("Tool: {}", tool);
-    //     },
-    //     Some(("test", _sub_matches)) => {
-    //         println!("Test");
-            // if name.get_one("sonar") {
-            //     println!("Scan sonar");
-            //     secz::commands::scan::sonarRun();
-            // } else if name.get_one("trivy") {
-            //     println!("Scan trivy");
-            //     // secz::commands::scan::Trivy();
-            // } else {
-            //     eprintln!("Unknown tool");
-            // }
-            // Some(("sonar", name.get_one("sonar"))) => {
-            //     println!("Scan sonar");
-            //     secz::commands::scan::sonarRun();
-            // },
-            // Some(("trivy", name.get_one("trivy"))) => {
-            //     println!("Scan trivy");
-            //     // secz::commands::scan::Trivy();
-            // },
-        // },
-        // _ => {
-        //     eprintln!("Unknown command");
-        // }
-    // }
+    let sub_matches = command.subcommand_matches("scan")
+        .expect("Expected 'scan' subcommand");
 
+    let tool = sub_matches.subcommand_name().unwrap_or("other");
+    println!("Tool: {:?}", tool);
+}
     // match cli.get_matches().subcommand() {
     //     secz::commands::scan::ScanCommands::ToolType(tool) => {
     //         if let Some(("tool", matches)) = cli.get_matches().subcommand() {
